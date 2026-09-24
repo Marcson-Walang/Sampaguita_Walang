4b09e6c7ebaf577736081f35af6d747b5a6f011f
Marcson-Walang <mawalang@carc.pshs.edu.ph>
2026-09-24 08:34:51 +0800

    Updated Distance Program with Notes and Readme.
    Added comments to clarify input and calculation steps.

--- Walang_Marcson_DistanceProgram.py
+++ Walang_Marcson_DistanceProgram.py
@@ -1,10 +1,13 @@
 import math
 
+# You get to enter the values of the coordinates using these.
 x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
 
+# It calculates the distance using the distance formula of two points,
 d = math.sqrt(math.pow(x2-x1,2)+math.pow(y2-y1,2))
 
+# This part gives the answer
 print(f"The distance between the two points is: {d:.2f}")
